e76e149a9e019871cc6a544a07479be8c4750314
Phillip Carter <pcarter@fastmail.com>
2021-11-05 18:02:01 -0700

    Create deterministic_sampler.go

---
+++ go-otel-samples/deterministic_sampler.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+
+	"go.opentelemetry.io/otel/attribute"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+var (
+	ErrInvalidSampleRate = errors.New("sample rate must be >= 1")
+)
+
+type deterministicSampler struct {
+	sampleRate          int
+	traceIDRatioSampler sdktrace.Sampler
+}
+
+func DeterministicSampler(sampleRate int) (*deterministicSampler, error) {
+	if sampleRate < 1 {
+		return nil, ErrInvalidSampleRate
+	}
+
+	return &deterministicSampler{
+		sampleRate:          sampleRate,
+		traceIDRatioSampler: sdktrace.TraceIDRatioBased(1.0 / float64(sampleRate)),
+	}, nil
+}
+
+func (ds *deterministicSampler) ShouldSample(p sdktrace.SamplingParameters) sdktrace.SamplingResult {
+	attrs := []attribute.KeyValue{
+		attribute.Int("SampleRate", int(ds.sampleRate)),
+	}
+
+	delegatedResult := ds.traceIDRatioSampler.ShouldSample(p)
+
+	return sdktrace.SamplingResult{
+		Decision:   delegatedResult.Decision,
+		Attributes: attrs,
+		Tracestate: delegatedResult.Tracestate,
+	}
+}
+
+func (ds *deterministicSampler) Description() string {
+	return "HoneycombDeterministicSampler"
+}
